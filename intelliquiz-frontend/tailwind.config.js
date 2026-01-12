3b9e0bc6bdbfa03f47fe9633f1fef2d27033a45a
reyes-simonejake <reyesjake262@gmail.com>
2026-01-12 09:16:49 +0800

    update the ui

--- intelliquiz-frontend/tailwind.config.js
+++ intelliquiz-frontend/tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blob: 'blob 7s infinite',
+      },
+      keyframes: {
+        blob: {
+          '0%, 100%': { transform: 'translate(0, 0) scale(1)' },
+          '33%': { transform: 'translate(30px, -50px) scale(1.1)' },
+          '66%': { transform: 'translate(-20px, 20px) scale(0.9)' },
+        },
+      },
+      delay: {
+        '2000': '2000ms',
+      }
+    },
   },
   plugins: [],
 }
